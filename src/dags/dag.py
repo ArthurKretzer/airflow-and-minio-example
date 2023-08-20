e50bca9f579b4ff5049b667f70b5484fb2e67e9b
Arthur Raulino Kretzer <arthur.raulino.kretzer@gmail.com>
2023-08-20 17:13:51 -0300

    recator: remove env varibles

--- src/dags/dag.py
+++ src/dags/dag.py
@@ -9,7 +9,6 @@ default_args = {
     "start_date": datetime(2023, 8, 12),
     "retries": 1,
     "retry_delay": timedelta(minutes=5),
-    # "env": {"stock_symbol_key": Variable.get("stock_symbol_key")},
 }
 
 dag = DAG(

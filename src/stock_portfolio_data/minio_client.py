f2c4c1a39c4649698d0ae403c8628dbc70eaafbc
Arthur Raulino Kretzer <arthur.raulino.kretzer@gmail.com>
2023-08-20 13:18:25 -0300

    fix: minio module import

--- src/stock_portfolio_data/minio_client.py
+++ src/stock_portfolio_data/minio_client.py
@@ -1,7 +1,8 @@
 from minio import Minio
 
 minio_client = Minio(
-    "stock-portfolio-data-minio-1:9000",
+    # "stock-portfolio-data-minio-1:9000",
+    "localhost:9000",
     access_key="XBJhoPKpkIqIPYmrgPGh",
     secret_key="UJQb2VWWystUFv6WffVtCxWvakYW0HTlrF9tz0yq",
     secure=False,  # Set to False if you are using HTTP instead of HTTPS
